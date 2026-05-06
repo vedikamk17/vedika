3de0e04ebf2ebe68d7988b2268a9bcced41a6346
vedikamk17 <87317862+vedikamk17@users.noreply.github.com>
2026-05-06 10:25:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-def add(a,b):
-  return a+b
+def add(a, b):
+    return a + b
 
 
-if __name__ == "__main__":
-  result=add(2,3)
-  print(result)
+if __name__ == "__main__":  # pragma: no cover
+    result = add(2, 3)
+    print(result)
